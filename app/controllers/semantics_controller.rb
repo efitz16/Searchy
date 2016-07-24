94dcccd82ac8e9af8f6cc2e0f313e328dbd7fd2a
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-07-24 17:14:12 -0400

    render results to bottom of form

--- app/controllers/semantics_controller.rb
+++ app/controllers/semantics_controller.rb
@@ -1,7 +1,7 @@
 class SemanticsController < ApplicationController
-  SEM_API_KEY = ''
+  SEM_API_KEY = 'SEM33C6880ADCC8CAF0FAD3D45F2BBE7037B'
 
-  SEM_SECRET = ''
+  SEM_SECRET = 'MTRkNTE4NTczMjkwYmRjZTVhZDAwZTU3MmJkMDZjZDk'
 
   def create
 
