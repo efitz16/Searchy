270bcc194a88d06b3de6a058555e5c2217b367f3
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-07-25 09:31:37 -0400

    revert

--- app/assets/javascripts/phone.js
+++ app/assets/javascripts/phone.js
@@ -1,131 +1,34 @@
-// var Phone = function(data){
-//   this.data = data;
-// }
-
-// Phone.prototype.getColors = function(){
-//   var colors = [];
-//   for(var i = 0; i < this.data.length; i++){
-//     colors.push(this.data[i]["values"]["color"]);
-//   }
-//   return colors;
-// }
-
-// Phone.prototype.colorSelection = function(){
-//   var colors = this.getColors();
-//   var option = "";
-//   for(var i = 0; i < colors.length; i++){
-//     option = option + "<option value='" +colors[i]+ "'" + ">"+colors[i]+ "</option>";
-//   }
-//   var selector = "<select name='colors' form='phone-form'>" +option+ "</select>";
-//   return selector;
-
-// }
-
-// Phone.prototype.createRequest = function(){
-
-//   var colors = this.getColors();
-
-
-
-//   var request = {
-
-//     "subject": "phones",
-//     "columns": [{
-//       "key": "price",
-//       "type": "NUMERIC",
-//       "goal": "min",
-//       "is_objective": true,
-//       "full_name": "Price",
-//       "range": {
-//         "low": 0,
-//         "high": 1000,
-//       },
-//       "format": "number:2"
-//     },
-//     {
-//       "type": "categorical",
-//       "key": "color",
-//       "full_name": "Color",
-//       "range": colors,
-//       "goal": "min",
-//       "preference": [],
-//       "is_objective": true
-//     },
-//     {
-//       "type": "NUMERIC",
-//       "key": "weight",
-//       "full_name": "Weight",
-//       "goal": "min",
-//       "range": {
-//         "low": 0,
-//         "high": 1000,
-//       },
-//       "is_objective": true
-//     }],
-//     "options": this.data
-//   };
-//   return request;
-// }
-
-// Phone.prototype.updatedRequest = function(data){
-
-//   var request = this.createRequest();
-
-//   request["columns"][0]["range"]["high"] = parseInt(data[0].value);
-//   request["columns"][1]["preference"].push(data[2].value);
-//   request["columns"][2]["range"]["high"] = parseInt(data[1].value);
-
-
-//   return request;
-
-// }
-
 var Phone = function(data){
   this.data = data;
 }
 
-Phone.prototype.getNames = function(){
-  var names = [];
-
-  for(var i = 1; i < this.data.length; i++){ // begins at one because first Object identifies the category
-    names.push(this.data[i]["name"])
+Phone.prototype.getColors = function(){
+  var colors = [];
+  for(var i = 0; i < this.data.length; i++){
+    colors.push(this.data[i]["values"]["color"]);
   }
-
-  return names;
+  return colors;
 }
 
-Phone.prototype.buildOptions = function(){
-  var options = [];
-
-  for(var i = 1; i < this.data.length; i++) {
-    hash = {};
-
-    hash["key"] = this.data[i]["id"];
-    hash["name"] = this.data[i]["name"];
-    hash["values"] = {};
-    hash["values"]["price"] = parseInt(this.data[i]["price"]);
-    hash["values"]["brand"] = this.data[i]["brand"];
-    hash["values"]["weight"] = parseInt(this.data[i]["weight"]);
-    
-    options.push(hash);
+Phone.prototype.colorSelection = function(){
+  var colors = this.getColors();
+  var option = "";
+  for(var i = 0; i < colors.length; i++){
+    option = option + "<option value='" +colors[i]+ "'" + ">"+colors[i]+ "</option>";
   }
-  return options;
+  var selector = "<select name='colors' form='phone-form'>" +option+ "</select>";
+  return selector;
 
 }
-
-Phone.prototype.getBrands = function(){
-  var brands = [];
-
-  for(var i = 1; i < this.data.length; i++){ // begins at one because first Object identifies the category
-    brands.push(this.data[i]["brand"])
-  }
-
-  return brands;  
-} 
 
 Phone.prototype.createRequest = function(){
-  
+
+  var colors = this.getColors();
+
+
+
   var request = {
+
     "subject": "phones",
     "columns": [{
       "key": "price",
@@ -135,9 +38,18 @@ Phone.prototype.createRequest = function(){
       "full_name": "Price",
       "range": {
         "low": 0,
-        "high": 0,
+        "high": 1000,
       },
       "format": "number:2"
+    },
+    {
+      "type": "categorical",
+      "key": "color",
+      "full_name": "Color",
+      "range": colors,
+      "goal": "min",
+      "preference": [],
+      "is_objective": true
     },
     {
       "type": "NUMERIC",
@@ -146,37 +58,24 @@ Phone.prototype.createRequest = function(){
       "goal": "min",
       "range": {
         "low": 0,
-        "high": 0,
+        "high": 1000,
       },
       "is_objective": true
-    },
-    {
-      "type": "categorical",
-      "key": "brand",
-      "full_name": "Brand",
-      "range": this.getBrands(),
-      "goal": "min",
-      "preference": [],
-      "is_objective": false
-    }], 
-    "options": this.buildOptions()
+    }],
+    "options": this.data
   };
-
   return request;
-} 
+}
 
 Phone.prototype.updatedRequest = function(data){
 
   var request = this.createRequest();
 
   request["columns"][0]["range"]["high"] = parseInt(data[0].value);
-  // request["columns"][2]["preference"].push(data[2].value);
-  request["columns"][1]["range"]["high"] = parseInt(data[1].value);
+  request["columns"][1]["preference"].push(data[2].value);
+  request["columns"][2]["range"]["high"] = parseInt(data[1].value);
+
 
   return request;
+
 }
-
-
-
-
-
--- app/assets/javascripts/runner.js
+++ app/assets/javascripts/runner.js
@@ -1,47 +1,33 @@
 $(document).ready(function(){
 	$('button').on('click', function() {
 		$('.main_container').hide();
-		$('body').html("<div class='category'><h1>Categories</h1><ul><li><button class='phones'>Phones</button></li><li><button class='computers'>Computers</button></li><li><button class='accessories'>Accessories</button></li><li><button class='clothing'>Clothing</button></li></ul></div>");
+		$('body').html("<div class='category'><h1>Categories</h1><ul><li><button class='phones'>Phones</button></li></ul></div>");
 
-		$('button').on('click', function() {
+		$('.phones').on('click', function() {
 			$('.category').hide();
-			var category = $(this).attr('class');
+			$('body').html("<button class='iphone'>Iphone</button></br></br><button class='samsung'>Samsungs</button>");
+
+			$('.iphone').on('click', function() {
+				$('.samsung').hide();
+				$('.iphone').hide();
 
 				$.ajax({
-					url: '/semantics?category=' + category,
-					type: 'GET'
+					url: '/semantics',
+					type: 'POST'
 				}).done(function(response) {
-					var prodForm;
-					switch(response[0]["type"]) {
-						case "computers":
-							prodForm = new Computer(response);
-
-							$('body').html("<form id='computer-form' class='choice-form' action='/semantics' method='post'>Max Price:<br><input type='number' name='maximum price' min='1' max='1000'><br>Max Weight:<br><input type='number' name='maximum weight' min='1' max='1000'><br>"+prodForm.kindSelection()+"<input type='submit' value='Submit'></form>");
-							break;
-						case "phones":
-							prodForm = new Phone(response);
-
-							$('body').html("<form id='phone-form' class='choice-form' action='/semantics' method='post'>Max Price:<br><input type='number' name='maximum price' min='1' max='1000'><br>Max Weight:<br><input type='number' name='maximum weight' min='1' max='1000'><br><input type='submit' value='Submit'></form>");
-							break;
-						case "clothes":
-							prodForm = new Clothing(response);
-
-							('body').html("<form id='clothing-form' class='choice-form' action='/semantics' method='post'>Max Price:<br><input type='number' name='maximum price' min='1' max='1000'><br>Max Weight:<br><input type='number' name='maximum weight' min='1' max='1000'><br><input type='submit' value='Submit'></form>");
-							break;
-						case "accessories":
-							prodForm = new Accessory(response);
-					}
-					$('.choice-form').on('submit', function(e) {
+					var phoneForm = new Phone(response);
+					$('body').html("<form id='phone-form' action='/semantics' method='post'>Max Price:<br><input type='number' name='maximum price' min='1' max='1000'><br>Max Weight:<br><input type='number' name='maximum weight' min='1' max='1000'><br>"+phoneForm.colorSelection()+"<input type='submit' value='Submit'></form>");
+					$('#phone-form').on('submit', function(e)  {
+						// var high_price = this
 						e.preventDefault();
-
-						prodForm = prodForm.updatedRequest($(this).serializeArray());
+						phoneForm = phoneForm.updatedRequest($(this).serializeArray());
 
 						$.ajax({
 							url: '/searches',
 							dataType: 'json',
 							contetType: 'application/json',
 							type: 'POST',
-							data: {data: JSON.stringify(prodForm)}
+							data: {data: JSON.stringify(phoneForm)}
 						}).done(function(response) {
 							// response.preventDefault();
 							
@@ -73,78 +59,21 @@ $(document).ready(function(){
 								var arr = [].concat.apply([], render);
 
 								for(var i = 0; i < arr.length; i++){
-									$('.choice-form').append('<p>'+arr[i]+'</p>');
+									$('#phone-form').append('<p>'+arr[i]+'</p>');
 								}
 
 						});
 
-					});
-					});
+					})
 				});
-			});
+				// $.ajax({
+				// 	url: "/searches",
+
+				// });
+			})
+
 		});
 
-
-// .done(function(response) {
-// 					var phoneForm = new Phone(response);
-// 					$('body').html("<form id='phone-form' action='/semantics' method='post'>Max Price:<br><input type='number' name='maximum price' min='1' max='1000'><br>Max Weight:<br><input type='number' name='maximum weight' min='1' max='1000'><br>"+phoneForm.colorSelection()+"<input type='submit' value='Submit'></form>");
-// 					$('#phone-form').on('submit', function(e)  {
-// 						// var high_price = this
-// 						e.preventDefault();
-// 						phoneForm = phoneForm.updatedRequest($(this).serializeArray());
-
-// 						$.ajax({
-// 							url: '/searches',
-// 							dataType: 'json',
-// 							contetType: 'application/json',
-// 							type: 'POST',
-// 							data: {data: JSON.stringify(phoneForm)}
-// 						}).done(function(response) {
-// 							// response.preventDefault();
-							
-// 							var key_option = {};
-
-// 							for(var i = 0; i < response["resolution"]["solutions"].length; i++) {
-// 								key_option[response["resolution"]["solutions"][i]["solution_ref"]] = response["resolution"]["solutions"][i]["status"];
-// 							}
-// 								var render = [];
-
-// 								var front = [];
-
-// 								var no_pref = [];
-
-
-// 								for (var i = 1; i <= response["problem"]["options"].length; i++) {
-// 									switch(key_option[i]){
-// 									case "FRONT":
-// 										front.push(response["problem"]["options"][i-1]["name"]);
-// 										break;
-// 									case "DOES_NOT_MEET_PREFERENCE":
-// 										no_pref.push(response["problem"]["options"][i-1]["name"]);
-// 									}
-// 								}
-
-// 								render.push(front);
-// 								render.push(no_pref);
-
-// 								var arr = [].concat.apply([], render);
-
-// 								for(var i = 0; i < arr.length; i++){
-// 									$('#phone-form').append('<p>'+arr[i]+'</p>');
-// 								}
-
-// 						});
-
-// 					})
-// 				});
-// 				// $.ajax({
-// 				// 	url: "/searches",
-
-// 				// });
-// 			})
-
-// 		});
-
 		
-// 	});
-// });
+	});
+});
