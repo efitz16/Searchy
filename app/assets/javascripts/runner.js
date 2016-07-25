ab88724b37596ec8a496457bf58e2ac3d467d466
Elizabeth Fitzpatrick <elizabethfitzpatrick16@gmail.com>
2016-07-25 01:00:30 -0400

    about to test wastosn request

--- app/assets/javascripts/runner.js
+++ app/assets/javascripts/runner.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 						case "computers":
 							prodForm = new Computer(response);
 
-							$('body').html("<form id='computer-form' action='/semantics' method='post'>Max Price:<br><input type='number' name='maximum price' min='1' max='1000'><br>Max Weight:<br><input type='number' name='maximum weight' min='1' max='1000'><br>"+prodForm.kindSelection()+"<input type='submit' value='Submit'></form>");
+							$('body').html("<form id='computer-form' class='choice-form' action='/semantics' method='post'>Max Price:<br><input type='number' name='maximum price' min='1' max='1000'><br>Max Weight:<br><input type='number' name='maximum weight' min='1' max='1000'><br>"+prodForm.kindSelection()+"<input type='submit' value='Submit'></form>");
 							break;
 						case "phones":
 							prodForm = new Phone(response);
@@ -27,6 +27,54 @@ $(document).ready(function(){
 						case "accessories":
 							prodForm = new Accessory(response);
 					}
+					$('.choice-form').on('submit', function(e) {
+						e.preventDefault();
+
+						prodForm = prodForm.updatedRequest($(this).serializeArray());
+
+						$.ajax({
+							url: '/searches',
+							dataType: 'json',
+							contetType: 'application/json',
+							type: 'POST',
+							data: {data: JSON.stringify(prodForm)}
+						}).done(function(response) {
+							// response.preventDefault();
+							
+							var key_option = {};
+
+							for(var i = 0; i < response["resolution"]["solutions"].length; i++) {
+								key_option[response["resolution"]["solutions"][i]["solution_ref"]] = response["resolution"]["solutions"][i]["status"];
+							}
+								var render = [];
+
+								var front = [];
+
+								var no_pref = [];
+
+
+								for (var i = 1; i <= response["problem"]["options"].length; i++) {
+									switch(key_option[i]){
+									case "FRONT":
+										front.push(response["problem"]["options"][i-1]["name"]);
+										break;
+									case "DOES_NOT_MEET_PREFERENCE":
+										no_pref.push(response["problem"]["options"][i-1]["name"]);
+									}
+								}
+
+								render.push(front);
+								render.push(no_pref);
+
+								var arr = [].concat.apply([], render);
+
+								for(var i = 0; i < arr.length; i++){
+									$('.choice-form').append('<p>'+arr[i]+'</p>');
+								}
+
+						});
+
+					});
 					});
 				});
 			});
