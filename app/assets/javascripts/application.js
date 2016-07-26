5f6decfdb026a1d6457f6de6686f7145582b3c53
jsgordon212 <jsgordon212@gmail.com>
2016-07-26 09:07:50 -0400

    reordered style sheets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
+//= require_tree .
 //= require react
 //= require react_ujs
 //= require components
 
-@import "bootstrap-sprockets"
 @import "bootstrap"
